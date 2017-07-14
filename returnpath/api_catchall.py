66cfdac0214fe2fed2d1aec309dac391d5d9d3bd
Trond Hindenes <trond.hindenes@rikstv.no>
2017-07-14 12:36:03 +0200

    also add request headers

--- returnpath/api_catchall.py
+++ returnpath/api_catchall.py
@@ -1,5 +1,6 @@
 from returnpath import api
 from flask_restful import Resource
+from flask import request
 import logging
 
 
@@ -10,10 +11,26 @@ class ApiCatchAll(Resource):
         if path is None:
             path = '/'
 
+        out_headers = {}
+        headers = request.headers.environ
+        for header in headers.keys():
+            if header.startswith('wsgi'):
+                pass
+            elif header.startswith('werkzeug'):
+                pass
+            else:
+                out_headers[header] = headers[header]
+
+
         if path.startswith('/'):
             pass
         else:
             path = '/' + path
-        return path
+
+        return_obj = {
+            'path': path,
+            'headers': out_headers
+        }
+        return return_obj
 
 api.add_resource(ApiCatchAll, '/', '/<path:path>')
