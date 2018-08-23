de7929e3f239c16ec85c4ca7f6fa505703e5d168
Trond Hindenes <trond@hindenes.com>
2018-08-23 17:00:25 +0200

    added support for POST

--- returnpath/api_catchall.py
+++ returnpath/api_catchall.py
@@ -37,4 +37,47 @@ class ApiCatchAll(Resource):
         }
         return return_obj
 
+    def post(self, path=None):
+        if path is None:
+            path = '/'
+
+        node_name = socket.gethostname()
+
+        out_headers = {}
+        headers = request.headers.environ
+
+        for header in headers.keys():
+            if header.startswith('wsgi'):
+                pass
+            elif header.startswith('werkzeug'):
+                pass
+            else:
+                out_headers[header] = headers[header]
+
+        try:
+            request_json = request.json
+        except:
+            request_json = None
+
+        try:
+            request_values = request.values
+        except:
+            request_values = None
+
+        if path.startswith('/'):
+            pass
+        else:
+            path = '/' + path
+
+        return_obj = {
+            'path': path,
+            'headers': out_headers,
+            'local_computer_name': node_name,
+            'request': {
+                'json_body': request_json,
+                'values': request_values
+            }
+        }
+        return return_obj
+
 api.add_resource(ApiCatchAll, '/', '/<path:path>')
